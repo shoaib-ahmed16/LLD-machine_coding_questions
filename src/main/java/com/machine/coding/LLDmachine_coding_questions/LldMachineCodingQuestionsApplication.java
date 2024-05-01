79d77290cd6fc2e2e138461d994a1efe468089cc
shoaib-ahmed16 <shoaib-ahmed16>
2024-05-01 07:06:44 +0530

    adding the base code

---
+++ src/main/java/com/machine/coding/LLDmachine_coding_questions/LldMachineCodingQuestionsApplication.java
@@ -0,0 +1,13 @@
+package com.machine.coding.LLDmachine_coding_questions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LldMachineCodingQuestionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LldMachineCodingQuestionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/machine/coding/LLDmachine_coding_questions/LldMachineCodingQuestionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.machine.coding.LLDmachine_coding_questions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LldMachineCodingQuestionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
